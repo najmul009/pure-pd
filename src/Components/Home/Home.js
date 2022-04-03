8fa0f6b29e3d0e258b61d88803835fcb370a3318
najmul009 <najmul.tk16@gmail.com>
2022-04-04 02:27:57 +0600

    banner added on home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,8 +3,15 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='banner'>
+            <div className="headlines">
+                <h1>Choose Your Next <span className='highlite'>Console</span></h1>
+                <h1>Analysis By <span className='highlite'>PurePD</span></h1>
+                <p>Reviews help you build a stellar online presence right where shoppers are researching their next purchase.Drive customer loyalty with learnings from consumer behavior and sentiment trends across locations and markets.</p>
+            </div>
+            <div className="bannerImg">
+                <img src="./Images/xbox.png" alt="" />
+            </div>
         </div>
     );
 };
